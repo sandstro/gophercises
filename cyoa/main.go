969229b63abed017d7823128a7f079dc520e9cad
Petri Sandström <hello@petrisandstrom.com>
2019-12-10 19:16:20 +0200

    GO-3: Add basic html template

--- cyoa/main.go
+++ cyoa/main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"gophercises/cyoa/stories"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+func getTemplate() string {
+	return `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>{{ .Title }}</title>
+		</head>
+		<body>
+			<header><h2>{{ .Title }}</h2></header>
+
+			<section>{{ range .Story }}<div>{{ . }}</div>{{ else }}<div><strong>No stories left :(</strong></div>{{ end }}</section>
+
+			<footer>
+				<div>{{ range .Options }}<a href="/{{ .Arc }}"><button>{{ .Text }}</button></a>{{ else }}<div>The end!</div>{{ end }}</div>
+				<div><a href="/"><button>Back to beginning</button></a></div>
+			</footer>
+		</body>
+	</html>`
+}
+
 func main() {
 	data := stories.ParseStoriesFromFile()
 
+	t, err := template.New("webpage").Parse(getTemplate())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		path := req.URL.Path
+		_ = path
+		t.Execute(w, data.Intro)
 	}
 
 	http.HandleFunc("/", helloHandler)
